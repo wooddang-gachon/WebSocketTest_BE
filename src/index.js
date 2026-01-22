c00ecb948ec417a43af155d95268e68f8b6a875b
wooddang-gachon <al457616@gachon.ac.kr>
2026-01-22 13:21:30 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// src/index.js
+// 메인 코드
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Express!!!!!!!");
+});
+
+app.listen(port, () => {
+  console.log("Example app listening on port 3000!");
+});
