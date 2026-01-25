38fdabea74c6868ee6eb12df4c9a75947dfc5c5d
wooddang-gachon <al457616@gachon.ac.kr>
2026-01-25 16:44:15 +0900

    readme 내용 업데이트

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 // src/index.js
 // 메인 코드
-
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello Express!!!!!!!");
+});
+app.get("/contacts", (req, res) => {
+  res.json({
+    message: "축하드립니다!",
+    date: new Date().toString(),
+  });
+  console.log("wow Attatched!");
 });
 
 app.listen(port, () => {
   console.log("Example app listening on port 3000!");
 });
+
